03cc0d13578f978682a4239f09c1d6b35c33bd95
mjrincon <mathewjrincon@gmail.com>
2016-06-07 13:28:38 -0400

    New Routes

--- blog/app/Http/routes.php
+++ blog/app/Http/routes.php
@@ -14,3 +14,29 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('waddup/{name}', function($name){
+	echo 'Waddup ' . $name;
+});
+
+// UPDATE ITEM
+Route::post('test', function(){
+	echo 'POSTED';
+});
+
+// CREATE ITEM
+Route::put('test', function(){
+	echo 'PUT';
+});
+// DELETE ITEM
+Route::delete('test', function(){
+	echo 'DELETED';
+});
+// READ ITEM
+Route::get('test', function(){
+	echo '<form action = "test" method="POST">';
+	echo '<input type ="submit" value="submit">';
+	echo '<input type="hidden" name="_token" value="' . csrf_token() . '">';
+	echo '<input type="hidden" name="_method" value="PUT">';
+	echo '</form>';
+});
