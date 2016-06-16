3cddcd93862460352cd206ac0cf1b9f113a28e1a
mjrincon <mathewjrincon@gmail.com>
2016-06-16 11:53:32 -0400

    tabs working

--- findzy/www/js/controllers.js
+++ findzy/www/js/controllers.js
@@ -1,28 +1,17 @@
-angular.module('starter.controllers', [])
+angular.module('findzy.controllers', ['ionic', 'findzy.services'])
 
-.controller('DashCtrl', function($scope) {})
+// Controller for discover page
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+.controller('DiscoverCtrl', function($scope) {})
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+
+// Controller for favorites page
+
+.controller('FavoritesCtrl', function($scope) {})
+
+
+// Controller for tabs
+
+.controller('TabsCtrl', function($scope) {})
